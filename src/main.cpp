f7c66f74cb762fda5bf7705900df896a85596799
howprice <howprice@users.noreply.github.com>
2026-03-03 17:39:02 +0000

    Apply reverb input and output volumes
    - Add PSX SPU reverb registers

--- src/main.cpp
+++ src/main.cpp
@@ -18,10 +18,11 @@ FIR Finite Impulse Response
 
 TODO
 ====
-- Implement reflection
+- Stereo
+- Reflection filters
 - Comb filters
 - All pass filters
-- Stereo
+- Save mixed (dry + wet) output
 
 */
 
@@ -55,6 +56,96 @@ static const s16 kFiniteImpulseResponse[] =
 	 0x0023,  0x0000, -0x000A,  0x0000,  0x0002,  0x0000, -0x0001,
 };
 static_assert(COUNTOF_ARRAY(kFiniteImpulseResponse) == 39);
+
+// Reverb Volume and Address Registers (R/W)
+//
+//   Port      Reg   Name    Type    Expl.
+//   1F801D84h spu   vLOUT   volume  Reverb Output Volume Left
+//   1F801D86h spu   vROUT   volume  Reverb Output Volume Right
+//   1F801DA2h spu   mBASE   base    Reverb Work Area Start Address in Sound RAM
+//   1F801DC0h rev00 dAPF1   disp    Reverb APF Offset 1
+//   1F801DC2h rev01 dAPF2   disp    Reverb APF Offset 2
+//   1F801DC4h rev02 vIIR    volume  Reverb Reflection Volume 1
+//   1F801DC6h rev03 vCOMB1  volume  Reverb Comb Volume 1
+//   1F801DC8h rev04 vCOMB2  volume  Reverb Comb Volume 2
+//   1F801DCAh rev05 vCOMB3  volume  Reverb Comb Volume 3
+//   1F801DCCh rev06 vCOMB4  volume  Reverb Comb Volume 4
+//   1F801DCEh rev07 vWALL   volume  Reverb Reflection Volume 2
+//   1F801DD0h rev08 vAPF1   volume  Reverb APF Volume 1
+//   1F801DD2h rev09 vAPF2   volume  Reverb APF Volume 2
+//   1F801DD4h rev0A mLSAME  src/dst Reverb Same Side Reflection Address 1 Left
+//   1F801DD6h rev0B mRSAME  src/dst Reverb Same Side Reflection Address 1 Right
+//   1F801DD8h rev0C mLCOMB1 src     Reverb Comb Address 1 Left
+//   1F801DDAh rev0D mRCOMB1 src     Reverb Comb Address 1 Right
+//   1F801DDCh rev0E mLCOMB2 src     Reverb Comb Address 2 Left
+//   1F801DDEh rev0F mRCOMB2 src     Reverb Comb Address 2 Right
+//   1F801DE0h rev10 dLSAME  src     Reverb Same Side Reflection Address 2 Left
+//   1F801DE2h rev11 dRSAME  src     Reverb Same Side Reflection Address 2 Right
+//   1F801DE4h rev12 mLDIFF  src/dst Reverb Different Side Reflect Address 1 Left
+//   1F801DE6h rev13 mRDIFF  src/dst Reverb Different Side Reflect Address 1 Right
+//   1F801DE8h rev14 mLCOMB3 src     Reverb Comb Address 3 Left
+//   1F801DEAh rev15 mRCOMB3 src     Reverb Comb Address 3 Right
+//   1F801DECh rev16 mLCOMB4 src     Reverb Comb Address 4 Left
+//   1F801DEEh rev17 mRCOMB4 src     Reverb Comb Address 4 Right
+//   1F801DF0h rev18 dLDIFF  src     Reverb Different Side Reflect Address 2 Left
+//   1F801DF2h rev19 dRDIFF  src     Reverb Different Side Reflect Address 2 Right
+//   1F801DF4h rev1A mLAPF1  src/dst Reverb APF Address 1 Left
+//   1F801DF6h rev1B mRAPF1  src/dst Reverb APF Address 1 Right
+//   1F801DF8h rev1C mLAPF2  src/dst Reverb APF Address 2 Left
+//   1F801DFAh rev1D mRAPF2  src/dst Reverb APF Address 2 Right
+//   1F801DFCh rev1E vLIN    volume  Reverb Input Volume Left
+//   1F801DFEh rev1F vRIN    volume  Reverb Input Volume Right
+//
+// All volume registers are signed 16bit (range -8000h..+7FFFh).
+// All src/dst/disp/base registers are addresses in SPU memory (divided by 8)
+// src/dst are relative to the current buffer address
+// disp registers are relative to src registers
+// The base register defines the start address of the reverb buffer (the end address is fixed, at 7FFFEh).
+// Writing a value to mBASE does additionally set the current buffer address to that value.
+// 
+// https://psx-spx.consoledev.net/soundprocessingunitspu/#spu-reverb-registers
+//
+struct ReverbRegisters
+{
+	// SPU registers
+	s16 vLOUT;
+	s16 vROUT;
+	u16 mBASE;
+
+	// Reverb unit registers
+	u16 dAPF1;
+	u16 dAPF2;
+	u16 vIIR;
+	u16 vCOMB1;
+	u16 vCOMB2;
+	u16 vCOMB3;
+	u16 vCOMB4;
+	u16 vWALL;
+	u16 vAPF1;
+	u16 vAPF2;
+	u16 mLSAME;
+	u16 mRSAME;
+	u16 mLCOMB1;
+	u16 mRCOMB1;
+	u16 mLCOMB2;
+	u16 mRCOMB2;
+	u16 dLSAME;
+	u16 dRSAME;
+	u16 mLDIFF;
+	u16 mRDIFF;
+	u16 mLCOMB3;
+	u16 mRCOMB3;
+	u16 mLCOMB4;
+	u16 mRCOMB4;
+	u16 dLDIFF;
+	u16 dRDIFF;
+	u16 mLAPF1;
+	u16 mRAPF1;
+	u16 mLAPF2;
+	u16 mRAPF2;
+	s16 vLIN;
+	s16 vRIN;
+};
 
 // Processes a whole buffer
 [[maybe_unused]]
@@ -195,6 +286,14 @@ static bool saveBuffer(const char* filename, s16* buffer, size_t numElements)
 	return true;
 }
 
+//
+// Saturates signed 32-bit value to signed 16-bit range
+//
+static s16 saturateS32toS16(s32 val)
+{
+	return (s16)Clamp(val, (s32)INT16_MIN, (s32)INT16_MAX);
+}
+
 int main(int argc, char** argv)
 {
 	// Parse filename from command line arguments
@@ -212,6 +311,18 @@ int main(int argc, char** argv)
 		LOG_ERROR("Failed to load input file: %s\n", inputFilename);
 		return EXIT_FAILURE;
 	}
+
+	ReverbRegisters regs{};
+
+	// #TODO: Set reg values to proper values e.g. from https://psx-spx.consoledev.net/soundprocessingunitspu/#reverb-examples or BIOS.
+
+	// For volumes:
+	// - INT16_MAX 0x7fff is max volume
+	// - INT16_MIN 0x8000 -32768 is max negative volume and will invert the signal
+	regs.vLIN = 0x7fff;
+	regs.vRIN = 0x7fff;
+	regs.vLOUT = 0x7fff;
+	regs.vROUT = 0x7fff;
 
 	// Create some intermediate buffers for debugging
 
@@ -237,9 +348,10 @@ int main(int argc, char** argv)
 		// Reverb is processed every other cycle i.e. at 22050 Hz
 		if ((inputSampleIndex & 1) == 0)
 		{
-			// Downsample from 44100 Hz to 22050 Hz
+			// Downsample from 44100 Hz to 22050 Hz to calculate the reverb unit input.
+			s32 LeftInput = 0; // input to reverb unit
+			s32 RightInput = 0;
 			{
-				s32 sum = 0;
 				for (size_t j = 0; j < COUNTOF_ARRAY(kFiniteImpulseResponse); j++)
 				{
 					// n.b. The filter is not centered so introduced a delay. To center the filter input index would need to
@@ -248,18 +360,35 @@ int main(int argc, char** argv)
 					s64 inputIndex = (s64)inputSampleIndex - (s64)j;
 
 					s16 inputSample = inputIndex >= 0 ? input[inputIndex] : 0;
-					sum += (s32)inputSample * (s32)kFiniteImpulseResponse[j];
+					LeftInput += (s32)inputSample * (s32)kFiniteImpulseResponse[j];
 				}
 
-				sum >>= 15; // Rescale by 0x8000
+				LeftInput >>= 15; // Rescale by 0x8000
 
 				HP_DEBUG_ASSERT(downsampledBufferLen < downsampledBufferCapacity); // should never fail by construction
-				downsampledBuffer[downsampledBufferLen++] = (s16)Clamp(sum, (s32)INT16_MIN, (s32)INT16_MAX); // Saturate to 16-bit signed range
+				downsampledBuffer[downsampledBufferLen++] = saturateS32toS16(LeftInput);
 			}
 
+			// Reverb
+
+			// Apply input volume vLIN, vRIN
+			s32 Lin = ((s32)regs.vLIN * LeftInput) >> 15; // / 0x8000;
+			s32 Rin = ((s32)regs.vRIN * RightInput) >> 15; // / 0x8000;
+			
 			// #TEMP: Just copy the downsampled buffer into the reverb output buffer for now, will implement actual reverb processing later.
+			// #TODO: Implement reverb chain
+			s32 Lout = Lin;
+			s32 Rout = Rin;
+
+			// Apply output volume vLOUT, vROUT
+			s32 LeftOutput = (Lout * (s32)regs.vLOUT) >> 15; // / 0x8000;
+			s32 RightOutput = (Rout * (s32)regs.vROUT) >> 15; // / 0x8000;
+
+			// Write to reverb output buffer at 22050 Hz
+			// Here, this is a full size buffer, but in the PSX SPU this could be a small circular buffer large enough to store the 39 samples needed for the FIR filter/
 			HP_DEBUG_ASSERT(reverbOutputBufferLen < reverbOutputBufferCapacitySamples); // should never fail by construction
-			reverbOutputBuffer[reverbOutputBufferLen] = downsampledBuffer[reverbOutputBufferLen];
+			reverbOutputBuffer[reverbOutputBufferLen] = saturateS32toS16(LeftOutput);
+			HP_UNUSED(RightOutput); // #TODO: Stereo
 			reverbOutputBufferLen++;
 		}
 
