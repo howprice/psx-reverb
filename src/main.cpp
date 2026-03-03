56b3d0f8e4ea32beae9321e7cd3bc7d41bbabf26
howprice <howprice@users.noreply.github.com>
2026-03-03 11:45:10 +0000

    Use PSX SPU 39-tap FIR filter to downsample from 44100 Hz to 22050 Hz

--- src/main.cpp
+++ src/main.cpp
@@ -9,11 +9,16 @@ x[n] is convolved with h[n] to produce y[n].
 
 Note that n just means some index into the signal. It could be written as i.
 
+For full convolution, the length of the output signal is equal to the length of the input signal, plus the length of the impulse response, minus one.
+
 FIR Finite Impulse Response
 
 */
 
 #include "Log.h"
+#include "hp_assert.h"
+#include "MathsHelpers.h"
+#include "ArrayHelpers.h"
 #include "Helpers.h" // HP_UNUSED
 #include "Types.h"
 
@@ -24,13 +29,50 @@ static void printUsage(const char* programName)
 	LOG_INFO("Usage: %s <filename>\n", programName);
 }
 
-static void process(const s16* input, size_t inputLength, s16* output, size_t outputLength)
+static bool downsample(
+	const s16* input, size_t inputLength,
+	s16* output, size_t outputCapacity, size_t& outputLength)
 {
-	// Placeholder: Copy input to output (identity operation)
-	for (size_t i = 0; i < outputLength; ++i)
+	// 39-tap PSX SPU FIR filter
+	// https://psx-spx.consoledev.net/soundprocessingunitspu/#reverb-buffer-resampling
+	// Each coefficient is a volume multipler N/0x8000, so after multiplying by the input sample, the result needs to be shift right by 15 to rescale.
+	// Note that every other sample except the peak is zero, so this is effectively a 20-tap filter with zeros interleaved for downsampling by 2.
+	static const s16 kImpulseResponse[] =
 	{
-		output[i] = (i < inputLength) ? input[i] : 0; // Zero-pad if output is longer than input
+		-0x0001,  0x0000,  0x0002,  0x0000, -0x000A,  0x0000,  0x0023,  0000,
+		-0x0067,  0x0000,  0x010A,  0x0000, -0x0268,  0x0000,  0x0534,  0000,
+		-0x0B90,  0x0000,  0x2806,  0x4000,  0x2806,  0x0000, -0x0B90,  0000,
+		 0x0534,  0x0000, -0x0268,  0x0000,  0x010A,  0x0000, -0x0067,  0000,
+		 0x0023,  0x0000, -0x000A,  0x0000,  0x0002,  0x0000, -0x0001,
+	};
+	static_assert(COUNTOF_ARRAY(kImpulseResponse) == 39);
+
+	outputLength = inputLength / 2; // Downsample by 2, so output length is half of input length
+	if (outputCapacity < outputLength)
+		return false;
+
+	// Resample from 44100 to 22050 Hz by convolving with impulse response and downsampling by 2.
+	for (size_t i = 0; i < outputLength; i++)
+	{
+		s32 sum = 0;
+		for (size_t j = 0; j < COUNTOF_ARRAY(kImpulseResponse); j++)
+		{
+			// Step input by 2 for downsampling.
+			// 
+			// n.b. The filter is not centered so introduced a delay. To center the filter input index would need to
+			// be offset by 19 (half of 39) but this would cause negative input index for the first 19 output samples,
+			// so we start with input index of 0 and let the filter introduce the delay.
+			s64 inputIndex = (s64)(2 * i) - (s64)j;
+
+			s16 inputSample = (inputIndex >= 0 && inputIndex < (s64)inputLength) ? input[inputIndex] : 0;
+			sum += (s32)inputSample * (s32)kImpulseResponse[j];
+		}
+
+		sum >>= 15; // Rescale by 0x8000
+		output[i] = (s16)Clamp(sum, (s32)INT16_MIN, (s32)INT16_MAX); // Saturate to 16-bit signed range
 	}
+
+	return true;
 }
 
 int main(int argc, char** argv)
@@ -75,10 +117,17 @@ int main(int argc, char** argv)
 	fclose(inputFile);
 	inputFile = nullptr;
 
-	size_t outputLength = inputLength; // #TODO: Determine output length based on convolution parameters
-	s16* output = new s16[outputLength];
+	// For full convolution, the length of the output signal is equal to the length of the input signal, plus the length of the impulse response, minus one.
+	// However we are downsampling by 2, so the output length is half of the input length. We can calculate the output capacity based on this.
+	size_t outputCapacity = inputLength / 2; // Downsample by 2, so output capacity is half of input length
+	s16* output = new s16[outputCapacity];
 
-	process(input, inputLength, output, outputLength);
+	size_t outputLength;
+	if (!downsample(input, inputLength, output, outputCapacity, /*out*/outputLength))
+	{
+		LOG_ERROR("Downsampling failed\n");
+		return EXIT_FAILURE;
+	}
 
 	// Save output
 	FILE* outputFile = fopen("output.bin", "wb");
